1cf620f2154892e9ec70dc9c6a1a52b448620c43
ROCKSTAR465 <kumaarsk390@gmail.com>
2025-01-26 03:34:06 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="video-subtitle-generator",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[
+        "streamlit==1.26.0",
+        "openai-whisper==20231106",
+        "moviepy==1.0.3",
+    ],
+)
